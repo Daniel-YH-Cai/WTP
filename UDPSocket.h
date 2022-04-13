516497dece03a20863ab3bc336d2075bbf76c040
YF-0111 <1155124411@link.cuhk.edu.hk>
2022-04-14 00:22:32 +0800

    init

---
+++ UDPSocket.h
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <iostream>
+#include "Packet.h"
+
+class UDPSocket
+{
+    int timeout = 10;
+    struct sockaddr_in si_other;
+    struct sockaddr_in si_me;
+    socklen_t len_other;
+    int fd;
+
+public:
+    UDPSocket()
+    {
+        memset(&si_other, 0, addrLen);
+        memset(&si_me, 0, addrLen);
+        len_other = addrLen;
+        fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    }
+
+    static const socklen_t addrLen = sizeof(sockaddr_in);
+    // bind to a port; should only be used in wReceiver
+    void bind_me(int port)
+    {
+        si_me.sin_family = AF_INET;
+        si_me.sin_port = htons(port);
+        si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+        bind(fd, (struct sockaddr *)&si_me, sizeof(si_me));
+    }
+    // set the host to which the packets will be sent to
+    // should only be used in wSender
+    void set_other(int port, const char *server)
+    {
+        si_other.sin_family = AF_INET;
+        si_other.sin_port = htons(port);
+        struct hostent *sp = gethostbyname(server);
+        memcpy(&si_other.sin_addr, sp->h_addr, sp->h_length);
+    }
+    // sent message to host;
+    // after receiving, si_other will automatically be set to the sender
+    // so set_other is not necessary while sending acks in wReceiver
+    int send(const char *message, int buffer_size)
+    {
+        std::cout << "Sending to host: " << get_other_addr() << " port: " << get_other_port() << "\n";
+        // this use strlen is error ,must use number
+        return sendto(fd, message, buffer_size, 0, (struct sockaddr *)&si_other, addrLen);
+    }
+    int receive(char *message, int buff_size)
+    {
+        int bytes = recvfrom(fd, message, buff_size, 0, (struct sockaddr *)&si_other, &len_other);
+        std::cout << "Received from host: " << get_other_addr() << " port: " << get_other_port() << "\n";
+        return bytes;
+    }
+
+    // receiving  a maximum of buff_size bytes into message in 500ms
+    // return -1 if do not receive any data in this period
+    // else return the amount of data received
+    int receiveTimeout(char *message, int buff_size)
+    {
+        int result = 0;
+        bool getSomeData = false;
+        int total_received = 0;
+        for (int i = 0; i < 20; i++)
+        {
+            result = recvfrom(fd, message + total_received, buff_size - total_received, MSG_DONTWAIT, (struct sockaddr *)&si_other, &len_other);
+            std::cout << "Received: " << result << "\n";
+            if (result == -1)
+                ;
+            else
+            {
+                getSomeData = true;
+                total_received = total_received + result;
+                if (total_received = buff_size)
+                {
+                    break;
+                }
+            }
+            // unit:milliseconds
+            usleep(25000);
+        }
+        if (!getSomeData)
+        {
+            return -1;
+        }
+        std::cout << "Received: " << total_received << "\n";
+        return total_received;
+    }
+    int get_other_port()
+    {
+        return ntohs(si_other.sin_port);
+    }
+    const char *get_other_addr()
+    {
+        return inet_ntoa(si_other.sin_addr);
+    }
+
+    void sendPacket(Packet p)
+    {
+        int buffer_size = 16 + p.get_length(); // header + data
+        char *buffer = (char *)malloc(sizeof(char) * (16 + p.length));
+        p.serialize(p, buffer);
+        this->send(buffer, buffer_size);
+    }
+
+    // bool: false if time out;
+    bool receivePacket(Packet *p)
+    {
+        char buffer[1472] = {0};
+        int buffer_size = this->receive(buffer, 1472);
+        Packet::deserialize(p, buffer, buffer_size);
+        return true;
+    }
+    ~UDPSocket()
+    {
+        close(fd);
+    }
+};
---
+++ wReceiver.cpp
@@ -0,0 +1,123 @@
+#include "UDPSocket.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class BatchReceiver
+{
+    UDPSocket *s;
+    // windows base: the first chunk in the windows;
+    // the next packet to be send and to wait for ack
+    string file;
+    ofstream logfile;
+    int window_size;
+    Packet **window;
+    int fileCount;
+    // the number of packets already sent and are waiting for ack
+
+public:
+    BatchReceiver(int ws, const char *filename, const char *log)
+    {
+        s = nullptr;
+        window_size = ws;
+        window = (Packet **)malloc(ws * sizeof(Packet *));
+        for (int i = 0; i < ws; i++)
+        {
+            window[i] = NULL;
+        }
+        logfile = ofstream(log);
+        file = filename;
+        file.seekg(0, ios::end);
+        // length = file.tellg();
+        file.seekg(0, ios::beg);
+        fileCount = 0;
+    }
+
+    void set_UDPSocket(UDPSocket *sock)
+    {
+        cout << "Socket set!\n";
+        s = sock;
+    }
+
+    void writeFile(ofstream file)
+    {
+        file.write(receiver.window[0].data, receiver.window[0].get_length());
+        for (int i = 0; i < receiver.window_size - 1; i++)
+        {
+            receiver.window[i] = receiver.window[i + 1];
+        }
+        receiver.window[receiver.window_size - 1] = NULL;
+    }
+
+}
+
+int
+main()
+{
+    if (argc != 5)
+    {
+        cout << "Usage: ./wSender\n";
+        return 0;
+    }
+    int port = atoi(argv[1]);
+    int window_size = atoi(argv[2]);
+    UDPSocket udp;
+    udp.bind_me(port);
+    // receiver.bind_me(8888);
+    cout << "Start receicing\n";
+    BatchReceiver receiver = new BatchReceiver(atoi(argv[2]), argv[3], argv[4]);
+    receiver.set_UDPSocket(udp);
+
+    while (true)
+    {
+        Packet p = new Packet();
+        receiver.s->receivePacket(&p);
+        logfile << p.get_type() << " " << p.get_seqNum()
+                << " " << p.get_length() << " " << p.get_checksum() << "\n";
+        if (p.get_type() == 0)
+        {
+            ofstream file = ofstream(receiver.file + "/FILE-" + to_string(receiver.fileCount++) + "out");
+            int num = 0;
+            int max = 0;
+
+            while (true)
+            {
+                Packet pData = new Packet();
+                receiver.s->receivePacket(&pData);
+                logfile << pData.get_type() << " " << pData.get_seqNum()
+                        << " " << pData.get_length() << " " << pData.get_checksum() << "\n";
+                // For each packet received
+                if (pData.checkSum())
+                {
+                    if (pData.get_seqNum() == num)
+                    {
+                        // if (pData.get_seqNum() > max)
+                        // {
+                        //     max = pData.get_seqNum();
+                        // }
+
+                        receiver.window[0] = &pData;
+                        while (receiver.window[0] != NULL)
+                        {
+                            receiver.writeFile(file);
+                            num += 1;
+                        }
+                        Packet newP = new Packet(num);
+                        s->sendPacket(newP);
+                    }
+                    else
+                    {
+                        if (pData.get_seqNum() > num && pData.get_seqNum() <= num + receiver.window_size)
+                        {
+                            receiver.window[pData.get_seqNum() - num] = &pData;
+                        }
+                        Packet newP = new Packet(num);
+                        s->sendPacket(newP);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
